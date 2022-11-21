fb403c3acc782d71a993388785b9ca4e19d5b8ba
kimgeonil <kji8922@naver.com>
2022-11-21 22:39:11 +0900

    Test deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
 REPOSITORY=/home/ubuntu/merge-frontend
 PROJECT_NAME=merge-frontend
 
 cd $REPOSITORY
 
 sudo cp ../env/merge-frontend/.env.production .env.production
-sudo /home/ubuntu/.nvm/versions/node/v16.14.2/bin/npm run production:start
+sudo /usr/bin/pm2 startOrReload ecosystem.config.js --env production
