f267f6f9a38c2d841c0df22eaa1354310f1e9a95
kimgeonil <kji8922@naver.com>
2022-11-21 22:56:23 +0900

    Change news api

--- src/services/news/api.ts
+++ src/services/news/api.ts
@@ -1,7 +1,7 @@
 import {API} from "../../config/api";
 
 export const getNewsListApi = async () => {
-  const { data } = await API.get('http://localhost:3000/api/news', {
+  const { data } = await API.get('/api/news', {
     // baseURL: '/api'
   });
   return data.data;
