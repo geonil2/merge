0b285d2c7414748ca651f650470c6157227b79b6
kimgeonil <kji8922@naver.com>
2022-11-21 18:11:16 +0900

    Add npm route

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ PROJECT_NAME=merge-frontend
 
 cd $REPOSITORY
 sudo cp ../env/merge-frontend/.env.production .env.production
-sudo npm run production:start
+sudo /home/ubuntu/.nvm/versions/node/v16.14.2/bin/npm run production:start
