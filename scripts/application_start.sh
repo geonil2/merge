29a4aaa008676d8f92b875dca541f10b1f542fc4
kimgeonil <kji8922@naver.com>
2022-11-23 13:41:17 +0900

    Change application_start

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,9 +6,5 @@ sudo chmod -R 777 REPOSITORY
 
 cd $REPOSITORY
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-cp ../env/merge/.env .env
+sudo cp ../env/merge/.env .env
 npm run start
