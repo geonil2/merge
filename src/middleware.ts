06b3f43d5063b74043df5997aad98ddd9ac85dd8
kimgeonil <kji8922@naver.com>
2022-11-23 21:38:47 +0900

    Update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,10 @@ import { NextResponse } from "next/server";
 
 export const middleware = async (request: NextRequest) => {
   if (request.nextUrl.pathname.startsWith('/writing')) {
-    if (!request.cookies.get('next-auth.session-token' || '__Secure-next-auth.session-token')) {
+    if (
+      !!request.cookies.get('next-auth.session-token') &&
+      !!request.cookies.get('__Secure-next-auth.session-token')
+    ) {
       return NextResponse.rewrite(new URL("/", request.url));
     }
   }
