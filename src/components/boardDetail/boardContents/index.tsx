7ed80759c2fd7f1e0fbd6cb8b28f5aad47752735
kimgeonil <kji8922@naver.com>
2022-11-28 11:10:52 +0900

    Remove board contents scroll

--- src/components/boardDetail/boardContents/index.tsx
+++ src/components/boardDetail/boardContents/index.tsx
@@ -81,7 +81,7 @@ const BoardContents: React.FC<Props> = ({ contents, userId }) => {
             </UserText>
           </Owner>
           <HeaderRight>
-            <LikesCount>추천 {contents.likes?.length}</LikesCount>
+            <LikesCount>좋아요 {contents.likes?.length}</LikesCount>
             <UpdateDeleteButtonWrapper
               owner={contents.owner?._id}
               onClickUpdateButton={onClickUpdateButton}
@@ -104,7 +104,7 @@ const BoardContents: React.FC<Props> = ({ contents, userId }) => {
               <path
                 d="M313.4 32.9c26 5.2 42.9 30.5 37.7 56.5l-2.3 11.4c-5.3 26.7-15.1 52.1-28.8 75.2H464c26.5 0 48 21.5 48 48c0 25.3-19.5 46-44.3 47.9c7.7 8.5 12.3 19.8 12.3 32.1c0 23.4-16.8 42.9-38.9 47.1c4.4 7.2 6.9 15.8 6.9 24.9c0 21.3-13.9 39.4-33.1 45.6c.7 3.3 1.1 6.8 1.1 10.4c0 26.5-21.5 48-48 48H294.5c-19 0-37.5-5.6-53.3-16.1l-38.5-25.7C176 420.4 160 390.4 160 358.3V320 272 247.1c0-29.2 13.3-56.7 36-75l7.4-5.9c26.5-21.2 44.6-51 51.2-84.2l2.3-11.4c5.2-26 30.5-42.9 56.5-37.7zM32 192H96c17.7 0 32 14.3 32 32V448c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V224c0-17.7 14.3-32 32-32z"/>
             </svg>
-            <div>추천 {contents.likes?.length}</div>
+            <div>좋아요 {contents.likes?.length}</div>
           </Likes>}
         </ContentsBody>
       </Container>
@@ -197,7 +197,6 @@ const Description = styled.div`
   max-height: 1000px;
   font-size: 14px;
   line-height: 160%;
-  overflow: scroll;
   margin-top: 30px;
   img {
     max-width: 100%;
